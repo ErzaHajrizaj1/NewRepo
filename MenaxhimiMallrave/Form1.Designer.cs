66ccf612a819196e7aedc90bffc69fe3d41a7dea
erza hajrizaj <erza.hajrizaj@infotech-kos.com>
2020-04-22 14:56:37 +0200

    Revert "e"
    This reverts commit b9e0264b2a838b65fea5e484086c0f5a218b1ff4.

--- MenaxhimiMallrave/Form1.Designer.cs
+++ MenaxhimiMallrave/Form1.Designer.cs
@@ -28,56 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(82)))), ((int)(((byte)(96)))));
-            this.pictureBox1.Location = new System.Drawing.Point(174, -2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(759, 43);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Items.AddRange(new object[] {
-            "Regjistrimi i Klientave",
-            "Regjistrimi i Puntorëve",
-            "Regjistrimi i Shitjeve",
-            "Regjistrimi i Furnizimeve",
-            "Regjistrimi i Ngarkimeve",
-            "Regjistrimi i Shkarkimeve"});
-            this.listBox1.Location = new System.Drawing.Point(1, -2);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(177, 596);
-            this.listBox1.TabIndex = 2;
-            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(926, 588);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(800, 483);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
