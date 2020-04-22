b9e0264b2a838b65fea5e484086c0f5a218b1ff4
erza hajrizaj <erza.hajrizaj@infotech-kos.com>
2020-04-22 13:49:27 +0200

    e

--- MenaxhimiMallrave/Form1.cs
+++ MenaxhimiMallrave/Form1.cs
@@ -16,5 +16,48 @@ namespace MenaxhimiMallrave
         {
             InitializeComponent();
         }
+
+        private void item1_Click(object sender, System.EventArgs e)
+        {
+            // Add event handler code here.
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                if (listBox1.SelectedItem.ToString()== "Regjistrimi i Klientave")
+                {
+                 RegjistrimiKlientave kmsh = new RegjistrimiKlientave();
+                kmsh.MdiParent = this;
+                ShowForm(kmsh);
+                }
+            }
+        }
+        public void ShowForm(Form frm)
+        {
+            bool IsOpen = false;
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.Text == frm.Text)
+                {
+                    IsOpen = true;
+                    f.Focus();
+                    break;
+                }
+            }
+
+            if (IsOpen == false)
+            {
+                //Form2 f2 = new Form2();
+                frm.MdiParent = this;
+                frm.Show();
+            }
+        }
     }
 }
